b12e257b8a7e13a7a81f2b6861680d157ba1697b
Jhonny Knaak de Vargas <jhonny.vargas@acad.pucrs.br>
2019-01-21 14:20:27 -0200

    fixed window problem

--- OrionUO/OrionWindow.cpp
+++ OrionUO/OrionWindow.cpp
@@ -685,3 +685,13 @@ bool COrionWindow::OnUserMessages(const UserEvent &ev)
 
     return true;
 }
+
+void COrionWindow::SetWindowResizable(const bool windowResizable) const
+{
+    SDL_SetWindowResizable(m_window, windowResizable ? SDL_TRUE : SDL_FALSE);
+}
+
+void COrionWindow::RestoreWindow() const
+{
+    SDL_RestoreWindow(m_window);
+}
